3ecb98c3cec6ed10440f2ba4fd1b3d2216b4ebbd
Tarık Köprülü <tarikk3545@gmail.com>
2019-11-07 16:18:22 +0300

    HTTP Connection with Axios, foundations of the app using Semantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values').then((response) => {
+      this.setState({
+        values: response.data
+      });
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactive</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((item: any) => {
+            return (<List.Item key={item.id}>{item.name}</List.Item>);
+          })}
+        </List>
+      </div>
+    );
+  }
 }
 
 export default App;
